dc5946a87111eeb073fa06e4afdc21c76ef9dc5f
Oliver Schmid <oliver.schmd@gmail.com>
2012-12-09 10:02:24 -0800

    \!appengine build tags

--- context.go
+++ context.go
@@ -1,3 +1,5 @@
+// +build !appengine
+
 // Copyright 2011 Google Inc. All rights reserved.
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
