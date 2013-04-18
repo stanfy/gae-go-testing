3abfd32747a231607d1028f4cdc9a73dc17549b5
Roman Mazur <mazur.roman@gmail.com>
2013-04-18 13:58:00 +0300

    Add init package

---
+++ appenginetestinit/appenginetestinit.go
@@ -0,0 +1,66 @@
+package appenginetestinit
+
+import (
+	"os"
+	"code.google.com/p/goprotobuf/proto"
+	"log"
+	"encoding/base64"
+	"strings"
+	"net/http"
+)
+
+var (
+	SavedHttpTransport = http.DefaultTransport
+	SavedHttpClient = http.DefaultClient
+)
+
+// Call this function from your test package init() function
+func Use() {
+	// nothing here
+}
+
+func init() {
+	if strings.LastIndex(os.Args[0], "test") != len(os.Args[0]) - len("test") {
+		// not a test environment
+		SavedHttpTransport = nil
+		SavedHttpClient = nil
+		return
+	}
+
+	// push AppEngine Go Instance config to stdin
+
+	port := int32(8080)
+	dc := "/"
+	id := "uatoday-instance"
+	config := Config{
+		AppId: []byte("test"),
+		VersionId: []byte("test"),
+		ApplicationRoot: []byte("."),
+		ApiPort: &port,
+		Datacenter: &dc,
+		InstanceId: &id,
+	}
+
+	bytes, err := proto.Marshal(&config)
+	if err != nil { panic(err) }
+	output := base64.StdEncoding.EncodeToString(bytes)
+
+	dir := os.TempDir()
+	fo, err := os.Create(dir + "/appcfg.proto")
+	if err != nil { panic(err) }
+
+	n := len(output)
+	log.Printf("Write %d bytes of config\n", n)
+	for n > 0 {
+		cnt, err := fo.Write([]byte(output[len(output) - n : len(output)]))
+		if err != nil { panic(err) }
+		n -= cnt
+	}
+
+	if err = fo.Close(); err != nil {
+		panic(err)
+	}
+
+	os.Stdin, err = os.Open(dir + "/appcfg.proto")
+	if err != nil { panic(err) }
+}
